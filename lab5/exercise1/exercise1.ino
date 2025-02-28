c087d730ba4ef60292000accd08e68e1c5e052ac
Dave Patrick Caberto <davecruz48@gmail.com>
2025-02-28 12:33:00 +0800

    updated

---
+++ lab5/exercise1/exercise1.ino
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+int ledpin = 9;
+float potentiometermaxvalue = 5000;
+float resistance;
+float voltage;
+float brightness;
+void setup()
+{
+    pinMode(ledpin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int potentiometer = analogRead(0);
+    float potentiometervoltage = potentiometer * (5.0 / 1023.0);
+    resistance = ((potentiometermaxvalue / 1023) * potentiometer);
+    brightness = potentiometer / 4;
+    float LEDvoltage = (5 * brightness) / 255;
+
+    Serial.print("resistance =");
+    Serial.println(resistance);
+
+    Serial.print("potentiometer voltage =");
+    Serial.println(5 - potentiometervoltage);
+
+    Serial.print("LED voltage =");
+    Serial.println(LEDvoltage);
+
+    Serial.println();
+
+    analogWrite(ledpin, brightness);
+}
